2d635c306e5350a589121923ad9d5cf4bb58d5ba
shizentaiga <tshizen2506@gmail.com>
2026-07-10 15:24:18 +0900

    initial commit: Hono on AWS Lambda

--- lib/aws-test-stack.ts
+++ lib/aws-test-stack.ts
@@ -1,16 +1,31 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigwv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 
 export class AwsTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 1. HonoアプリをLambda関数として定義
+    const honoFunction = new lambda.NodejsFunction(this, 'HonoHandler', {
+      entry: 'src/index.ts', // プロジェクトルートからのパス
+      handler: 'handler',
+      runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 2. API Gateway (HTTP API) を作成
+    const httpApi = new apigwv2.HttpApi(this, 'HonoApi');
+
+    // 3. API GatewayとLambdaを紐付ける
+    httpApi.addRoutes({
+      path: '/{proxy+}',
+      methods: [apigwv2.HttpMethod.ANY],
+      integration: new integrations.HttpLambdaIntegration('HonoIntegration', honoFunction),
+    });
+
+    // デプロイ後にURLを表示するための出力設定
+    new cdk.CfnOutput(this, 'ApiUrl', { value: httpApi.apiEndpoint });
   }
 }
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/aws-lambda'
+
+const app = new Hono()
+app.get('/', (c) => c.text('Hello Hono!'))
+
+export const handler = handle(app)
